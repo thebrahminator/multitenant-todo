3ea7ea7f1cac815f08caa1c683b97176d7f14447
thebrahminator <vishwa.nath@outlook.com>
2018-10-16 13:46:51 +0530

    Subdomain works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 app.secret_key = '\/Y/-\$!$/-\|>!(|<'
@@ -14,9 +15,18 @@ Go to /etc/hosts and add a rule.
 127.0.0.1 local.com
 '''
 app.config['SERVER_NAME'] = 'local.com:5000'
+
+
 @app.route('/')
 def index():
     return 'Hello World'
+
+
+@app.route('/getSubdomain', subdomain='<tenant>')
+def subdomain(tenant):
+
+    subdomain_ = urlparse(request.url).hostname.split('.')[0]
+    return f'Subdomain is {subdomain_}'
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
